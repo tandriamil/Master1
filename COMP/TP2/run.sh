e58c824f532a3e9932f4cd5319a60db1fc0d81c1
Andriamilanto Tompoariniaina <contact@napi.fr>
2015-11-06 19:59:11 +0100

    Putting the run.sh script and the local definition of the workspace

---
+++ COMP/TP2/run.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Run script of the duo ANDRIAMILANTO Tompoariniaina and IHSINE Azzeddine
+
+# Some local definitions
+src_dir=./src
+bin_dir=./bin
+test_dir=./tests
+
+# If bin dir doesn't exist
+if [ ! -d $bin_dir ]
+then
+	mkdir $bin_dir
+fi
+
+# Some local definitions
+if [ -n $LOCAL_WORKSPACE ]
+then
+	antlr_jar=$HOME/Librairies/antlr-3.5.jar
+else
+	antlr_jar=/share/m1info/COMP/antlr/antlr-3.5.jar
+fi
+
+# Generate java files from grammars
+echo "Generating java files from grammars"
+java -cp $bin_dir:$antlr_jar org.antlr.Tool $src_dir/VSLTreeParser.g
+java -cp $bin_dir:$antlr_jar org.antlr.Tool $src_dir/VSLParser.g
+
+# Compile sources
+echo "\nCompile sources"
+javac -cp $bin_dir:$antlr_jar -d $bin_dir $src_dir/*.java
+
+# Run tests
+echo "\nHere we should run tests"
