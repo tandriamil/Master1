4604d4cd7a3796bc8b0e88b661c74599f8d76883
Andriamilanto Tompoariniaina <contact@napi.fr>
2015-11-26 12:34:06 +0100

    Test condition and test lock to tests

--- SGP/TP nachos/test/test_condition.c
+++ SGP/TP nachos/test/test_condition.c
@@ -1,6 +1,6 @@
 /**
  *
- * Simple test case for the conditions
+ * Simple test case for the conditions synchronisation
  *
  * ANDRIAMILANTO Tompoariniaina
  * IHSINE Azzeddine
@@ -14,10 +14,76 @@
 // Nachos system calls
 #include "userlib/syscall.h"
 
+// Table used here
+#define NUM 30
+int tab[NUM];
+
+
+// Function to fill the table
+void fillThread(int c) {
+
+	// Transform the lock
+	CondId cond = (int)c;
+
+	tab[0] = 0;
+	tab[1] = 1;
+	
+	int i;
+	int j;
+	for (i = 2; i < NUM; i++) {
+		tab[i] = 0;
+
+		for (j = (i-1); j > 0; j--) {
+			tab[i] += tab[j];
+		}
+	}
+
+	// Signal the cond
+	if (CondSignal(cond) < 0)
+		n_printf("Error signal condition");
+}
+
+
+// Function to read the table
+void readThread(int c) {
+
+	// Transform the lock
+	CondId cond = (int)c;
+
+	// Wait our condition
+	if (CondWait(cond) < 0)
+		n_printf("Error condition wait");
+	
+	int i;
+	for (i = 0; i < NUM; i++) {
+		n_printf("Tab[" + i + "] = " + tab[i] + "\n");
+	}
+}
+
 
 /**
  * Main function
  */
 int main() {
-	
+
+	// We create a condition
+	CondId cond = CondCreate("TableCondition");
+
+	// We create a thread to fill it and one to read it
+	ThreadId thread_fill = newThread("FillThread", fillThread, (int)cond);
+	ThreadId thread_read = newThread("ReadThread", readThread, (int)cond);
+
+	// Wait the two threads before returning
+	if (Join(thread_fill) < 0)
+		n_printf("Error joining first thread");
+
+	if (Join(thread_read) < 0)
+		n_printf("Error joining second thread");
+
+	// Delete the condition
+	if (CondDestroy(cond) < 0)
+		n_printf("Error destroy lock");
+
+	// Exit the program
+	Exi(0);
 }
--- SGP/TP nachos/test/test_lock.c
+++ SGP/TP nachos/test/test_lock.c
@@ -10,9 +10,32 @@
  *
  */
 
-
 // Nachos system calls
 #include "userlib/syscall.h"
+#define MAX_THREADS
+ThreadId threads[MAX_THREADS];
+int i = 1;
+
+
+// Function to say who is each threads
+void whoAmI(int l) {
+
+	// Transform the lock
+	(LockId)lock = l;
+
+	// Take the lock
+	if (LockAcquire(lock) < 0)
+		n_printf("Error acquire lock");
+
+	// Wait and then display who he is
+	n_printf("I am the thread number " + i + "!\n");
+	i++;
+
+	// Release the lock
+	if (LockRelease(lock) < 0)
+		n_printf("Error release lock");
+
+}
 
 
 /**
@@ -20,4 +43,25 @@
  */
 int main() {
 	
+	// Create a lock
+	LockId lock = LockCreate("Lock");
+
+	// Create a lot of threads
+	int j;
+	for(j = 0; j < MAX_THREADS; j++) {
+		threads[j] = newThread("Threads j=" + j, whoAmI, (int)lock);
+	}
+
+	// Wait each threads
+	for (j = 0; j < MAX_THREADS; j++) {
+		if (Join(threads[j]) < 0)
+			n_printf("Error joining thread j=" + j);
+	}
+
+	// Destroy the lock
+	if (LockDestroy(lock) < 0)
+		n_printf("Error joining thread j=" + j);
+
+	// Exit the program
+	Exi(0);
 }
