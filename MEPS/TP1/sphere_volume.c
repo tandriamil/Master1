a44e6acbd261a557ebba516b6f6c62a45c139275
Andriamilanto Tompoariniaina <contact@napi.fr>
2016-01-28 21:05:37 +0100

    Added the TP1 and TP2 of MEPS

---
+++ MEPS/TP1/sphere_volume.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+/* ################################### Externs ################################### */
+/**
+ * 32-bits Random number generator U(0,1): MRG32k3a
+ * Author: Pierre L'Ecuyer,
+ * Source: 	Good Parameter Sets for Combined Multiple Recursive Random
+ *			Number Generators,
+ *			Shorter version in Operations Research,
+ *			47, 1 (1999), 159--164.
+ *
+ * I just edited the commentary and design of the code
+**/
+
+// Constants
+#define NORM 2.328306549295728e-10
+#define M1 4294967087.0
+#define M2 4294944443.0
+#define A12 1403580.0
+#define A13N 810728.0
+#define A21 527612.0
+#define A23N 1370589.0
+#define SEED 12345
+
+
+// Static variables
+static double s10 = SEED, s11 = SEED, s12 = SEED, s20 = SEED, s21 = SEED, s22 = SEED;
+
+
+/**
+ * Get a pseudo-random double number
+ * \return A pseudo-random double number
+ */
+double MRG32k3a() {
+
+	long k;
+	double p1, p2;
+
+	/* Component 1 */
+	p1 = A12 * s11 - A13N * s10;
+	k = p1 / M1;
+	p1 -= k * M1;
+	if (p1 < 0.0) p1 += M1;
+	s10 = s11;
+	s11 = s12;
+	s12 = p1;
+
+	/* Component 2 */
+	p2 = A21 * s22 - A23N * s20;
+	k = p2 / M2;
+	p2 -= k * M2;
+	if (p2 < 0.0) p2 += M2;
+	s20 = s21;
+	s21 = s22;
+	s22 = p2;
+
+	/* Combination */
+	if (p1 <= p2) return ((p1 - p2 + M1) * NORM);
+	else return ((p1 - p2) * NORM);
+}
+
+/* ################################### End of Externs ################################### */
+
+
+/**
+ * Main function to estimate the mean of exponential random variable
+ * 
+ * \param argc The number of aguments
+ * \param argv The arguments provided to the program
+ */
+int main(int argc, char** argv) {
+
+	// Check the arguments
+	if (argc != 2) {
+		printf("The correct syntax is: %s nb_runs\n", argv[0]);
+		return -1;
+	}
+
+	// The result to return and the random value got
+	double random_x, random_y, random_z, distance, result = 0, nb_runs = atoi(argv[1]);
+
+	// Runs nb_runs tries
+	int i;
+	for (i = 0; i < nb_runs; i++) {
+
+		// Get 3 random variables
+		random_x = MRG32k3a();
+		random_y = MRG32k3a();
+		random_z = MRG32k3a();
+
+		// Get the distance of each points
+		distance = sqrt(pow(0.5 - random_x, 2) + pow(0.5 - random_y, 2) + pow(0.5 - random_z, 2));
+
+		// If less than the radius of the sphere
+		if (distance <= 0.5) result += 1;
+	}
+
+	// Divide by the number of tries
+	result = result / nb_runs;
+
+	// Display the result
+	printf("Estimation of the volume of the sphere: %lf.\n", result);
+
+	// Exit correctly
+	return 0;
+}
---
+++ MEPS/TP2/function_surface.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+/* ################################### Externs ################################### */
+/**
+ * 32-bits Random number generator U(0,1): MRG32k3a
+ * Author: Pierre L'Ecuyer,
+ * Source: 	Good Parameter Sets for Combined Multiple Recursive Random
+ *			Number Generators,
+ *			Shorter version in Operations Research,
+ *			47, 1 (1999), 159--164.
+ *
+ * I just edited the commentary and design of the code
+**/
+
+// Constants
+#define NORM 2.328306549295728e-10
+#define M1 4294967087.0
+#define M2 4294944443.0
+#define A12 1403580.0
+#define A13N 810728.0
+#define A21 527612.0
+#define A23N 1370589.0
+#define SEED 12345
+
+
+// Static variables
+static double s10 = SEED, s11 = SEED, s12 = SEED, s20 = SEED, s21 = SEED, s22 = SEED;
+
+
+/**
+ * Get a pseudo-random double number
+ * \return A pseudo-random double number
+ */
+double MRG32k3a() {
+
+	long k;
+	double p1, p2;
+
+	/* Component 1 */
+	p1 = A12 * s11 - A13N * s10;
+	k = p1 / M1;
+	p1 -= k * M1;
+	if (p1 < 0.0) p1 += M1;
+	s10 = s11;
+	s11 = s12;
+	s12 = p1;
+
+	/* Component 2 */
+	p2 = A21 * s22 - A23N * s20;
+	k = p2 / M2;
+	p2 -= k * M2;
+	if (p2 < 0.0) p2 += M2;
+	s20 = s21;
+	s21 = s22;
+	s22 = p2;
+
+	/* Combination */
+	if (p1 <= p2) return ((p1 - p2 + M1) * NORM);
+	else return ((p1 - p2) * NORM);
+}
+
+/* ################################### End of Externs ################################### */
+
+// My constants
+#define C90 1.645
+#define C95 1.96
+#define C99 2.58
+
+
+/**
+ * Main function to estimate the mean of exponential random variable
+ * 
+ * \param argc The number of aguments
+ * \param argv The arguments provided to the program
+ */
+int main(int argc, char** argv) {
+
+	// Check the arguments
+	if (argc != 2) {
+		printf("The correct syntax is: %s nb_runs\n", argv[0]);
+		return -1;
+	}
+
+	// The result to return and the random value got
+	double variance, random_x, random_y, value, result = 0, nb_runs = atoi(argv[1]);
+
+	// Runs nb_runs tries
+	int i;
+	for (i = 0; i < nb_runs; i++) {
+
+		// Get 3 random variables
+		random_x = MRG32k3a();
+		random_y = MRG32k3a();
+
+		// Get the value of the function for the random x
+		value = 3.0 * pow(random_x, 2);
+
+		// If less than the radius of the sphere
+		if (random_y <= value) result += 1;
+	}
+
+	// Multiply the result by 3 (because we have a [1;3] grid and not a [1;1])
+	result *= 3;
+
+	// Get the variance
+	variance = sqrt((result - (nb_runs * pow((result / nb_runs), 2))) / (nb_runs - 1));
+
+	// Divide by the number of tries
+	result = result / nb_runs;
+
+	// Display the result
+	printf("Estimation of the mean: %lf.\n", result);
+	printf("Confidence interval with 90 percent rate: [%lf, %lf].\n", result - C90*(variance/sqrt(nb_runs)), result + C90*(variance/sqrt(nb_runs)));
+	printf("Confidence interval with 95 percent rate: [%lf, %lf].\n", result - C95*(variance/sqrt(nb_runs)), result + C95*(variance/sqrt(nb_runs)));
+	printf("Confidence interval with 99 percent rate: [%lf, %lf].\n", result - C99*(variance/sqrt(nb_runs)), result + C99*(variance/sqrt(nb_runs)));
+
+	// Exit correctly
+	return 0;
+}
