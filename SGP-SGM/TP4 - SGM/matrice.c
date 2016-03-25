a60e6c0c7d5e3127ed13f9b9a4b42f9adf53caa5
Andriamilanto Tompoariniaina <contact@napi.fr>
2016-03-25 09:57:30 +0100

    end'

--- SGP-SGM/TP4 - SGM/matrice.c
+++ SGP-SGM/TP4 - SGM/matrice.c
@@ -18,7 +18,7 @@
 #define PAGES_PER_MATRIX 1
 #define RAND_MIN_VALUE 0
 #define RAND_MAX_VALUE 100
-#define MATRIX_SIZE getpagesize() * PAGES_PER_MATRIX
+#define ROW_SIZE getpagesize() * PAGES_PER_MATRIX
 #define FILENAME "mmap.txt"
 
 
@@ -27,7 +27,8 @@
  * \return A random int
  */
 int random_int() {
-	return rand()%(RAND_MAX_VALUE - RAND_MIN_VALUE) + RAND_MIN_VALUE;
+	fprintf(stderr, "Random int generated = %d\n", (int)(rand()%(RAND_MAX_VALUE - RAND_MIN_VALUE) + RAND_MIN_VALUE));
+	return (int)(rand()%(RAND_MAX_VALUE - RAND_MIN_VALUE) + RAND_MIN_VALUE);
 }
 
 
@@ -41,7 +42,7 @@ int random_int() {
  * \return A pointer to the wanted memory cell
  */
 int * cell(int * beginning, int x, int y) {
-	return beginning + ((y * MATRIX_SIZE + x) * sizeof(int));
+	return beginning + y * ROW_SIZE + x;
 }
 
 
@@ -60,31 +61,37 @@ int main() {
 	// Correct execution
 	else {
 
-		fprintf(stderr, "File correctly created with fd = %d\n", file);
+		// Here, initialise the matrix
+		int i, j;
+		int matrix[ROW_SIZE][ROW_SIZE];
+		for (i = 0; i < ROW_SIZE; ++i) {
+			for (j = 0; j < ROW_SIZE; ++j) {
+				matrix[i][j] = random_int();
+			}
+		}
 
-		// We create a mapped file
-		int *mapped_file = (int *)mmap(0, MATRIX_SIZE * 2 * sizeof(int), PROT_READ|PROT_WRITE, MAP_SHARED, file, 0);
-		if (mapped_file == MAP_FAILED) fprintf(stderr, "Error during opening the mapped file\n");
+		// Write it in the file
+		if (write(file, matrix, ROW_SIZE * ROW_SIZE * sizeof(int)) == -1) fprintf(stderr, "Error during writing the matrix into the file named %s\n", FILENAME);
 
 		// Correct execution
 		else {
 
-			fprintf(stderr, "Mapped file correctly created at @%lo\n", (long)mapped_file);
+			fprintf(stderr, "File correctly created and initialised with fd = %d\n", file);
 
-			// We create our matrix
-			int i, j;
-			for (i = 0; i < MATRIX_SIZE; ++i) {
-				for (j = 0; j < MATRIX_SIZE; ++j) {
+			// We create a mapped file
+			int *mapped_file = (int *)mmap(0, ROW_SIZE * ROW_SIZE * sizeof(int), PROT_READ|PROT_WRITE, MAP_SHARED, file, 0);
+			if (mapped_file == MAP_FAILED) fprintf(stderr, "Error during opening the mapped file\n");
 
-					// Fill the matrix with random integers
-					*(cell(mapped_file, i, j)) = random_int();
-				}
-			}
+			// Correct execution
+			else {
 
-			// Check that the initialisation worked
-			for (i = 0; i < MATRIX_SIZE; ++i) {
-				for (j = 0; j < MATRIX_SIZE; ++j) {
-					fprintf(stderr, "%d\n", *(cell(mapped_file, i, j)));
+				fprintf(stderr, "Mapped file correctly created at @%p\n", mapped_file);
+
+				// Check that the initialisation worked
+				for (i = 0; i < ROW_SIZE; ++i) {
+					for (j = 0; j < ROW_SIZE; ++j) {
+						fprintf(stderr, "%d\n", *(cell(mapped_file, i, j)));
+					}
 				}
 			}
 		}
